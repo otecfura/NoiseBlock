338b3472dc062f459cdbab5df7de464b2dacd692
otecfura <fura.otec@gmail.com>
2024-09-26 14:36:25 +0200

    feat(com): added com

--- app/src/androidTest/java/com/aquasoup/barking/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/aquasoup/barking/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("cz.aquasoup.barking", appContext.packageName)
+        assertEquals("com.aquasoup.barking", appContext.packageName)
     }
 }
